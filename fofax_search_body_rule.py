47402a1a3acefe65fbb6ce302e1bea308ea9ee97
hecanfeng <hecanfeng@aliyun.com>
2022-05-09 15:09:43 +0800

    将目标添加fofa的搜索语句规则，使用fofax进行批量搜索

---
+++ fofax_search_body_rule.py
@@ -0,0 +1,13 @@
+label=""
+f = open("fofax_search_body_result.txt",encoding = "utf-8",mode="w")
+with open("target.txt",encoding="utf-8",mode="r") as fofa:
+    line = fofa.readlines()
+    i = 0
+    for line_list in line:
+        line_new = line_list.replace('\n', '')  # 将换行符替换为空('')
+        b = str(label)  # 主要是这一步 将之前列表数据转为str才能加入列表
+        line_new = 'body="' + line_new +'"' + b + '\n'
+        i += 1
+        print(line_new)
+        f.write(line_new)  # 写入一个新文件中
+f.close()
